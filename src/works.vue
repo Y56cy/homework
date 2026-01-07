4c920788386d25a9067e9c3635ae151635774659
paradise611 <1758573604@qq.com>
2026-01-07 23:07:03 +0800

    添加电影简介

--- src/works.vue
+++ src/works.vue
@@ -2,9 +2,11 @@
   <Nav />
   <Sakura />
   <headphoto />
+  <movies />
 </template>
 <script setup>
     import Nav from './components/Nav.vue';  
     import Sakura from './components/Sakura.vue';  
     import headphoto from './components/headphoto.vue';
+    import movies from './components/movies.vue';
 </script>
