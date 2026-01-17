d13b2cb3f96e07c311efff902c63b400d7d22670
paradise611 <1758573604@qq.com>
2026-01-18 01:43:40 +0800

    fix:修改了全局样式

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,3 @@ import Allfooter from './components/allfooter.vue';
 import Nav from './components/Nav.vue';
 
 </script>
-<style>
-/* 全局样式：去除默认边距 */
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-body {
-  background-color: #f5f7fa;
-}
-</style>
